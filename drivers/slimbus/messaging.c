1af40bfad2cf0df69fcc664f341886fd33350d96
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2021-06-25 16:54:45 +0530

    PENDING: slimbus: messaging: Check for valid transaction id for PM runtime
    In some usecases transaction ids are dynamically allocated inside
    the controller driver which have generic acknowledge responses.
    So check for txn->tid before refcounting pm_runtime.
    
    Also add required changes to refcount PM runtime get added
    in qcom_slim_ngd_xfer_msg_sync API in error path.
    
    Change-Id: Id4ad6da84e0f8d77bcb72e4c55dddea90aace869
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
    Signed-off-by: Viken Dadhaniya <quic_vdadhani@quicinc.com>

--- drivers/slimbus/messaging.c
+++ drivers/slimbus/messaging.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2011-2017, The Linux Foundation
+ * Copyright (c) 2011-2017, 2021 The Linux Foundation. All rights reserved.
  */
 
 #include <linux/slab.h>
@@ -123,14 +123,6 @@ int slim_do_transfer(struct slim_controller *ctrl, struct slim_msg_txn *txn)
 		 txn->mc <= SLIM_MSG_MC_RECONFIGURE_NOW))
 		clk_pause_msg = true;
 
-	if (!clk_pause_msg) {
-		ret = pm_runtime_get_sync(ctrl->dev);
-		if (ctrl->sched.clk_state != SLIM_CLK_ACTIVE) {
-			dev_err(ctrl->dev, "ctrl wrong state:%d, ret:%d\n",
-				ctrl->sched.clk_state, ret);
-			goto slim_xfer_err;
-		}
-	}
 	/* Initialize tid to invalid value */
 	txn->tid = 0;
 	need_tid = slim_tid_txn(txn->mt, txn->mc);
@@ -144,6 +136,15 @@ int slim_do_transfer(struct slim_controller *ctrl, struct slim_msg_txn *txn)
 			txn->comp = &done;
 		else
 			txn->comp = txn->comp;
+	}
+
+	if (!clk_pause_msg) {
+		ret = pm_runtime_get_sync(ctrl->dev);
+		if (ctrl->sched.clk_state != SLIM_CLK_ACTIVE) {
+			dev_err(ctrl->dev, "ctrl wrong state:%d, ret:%d\n",
+				ctrl->sched.clk_state, ret);
+			goto slim_xfer_err;
+		}
 	}
 
 	ret = ctrl->xfer_msg(ctrl, txn);
