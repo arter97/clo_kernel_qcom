00cc5bc9349245b2e716e912a695aeb4a6f80eee
Bhupesh Sharma <bhupesh.sharma@linaro.org>
2022-08-05 03:35:52 +0530

    FROMLIST: dt-bindings: thermal: Add qcom,qmi-tmd-device and qcom,tmd-device yaml bindings
    Add qcom,qmi-tmd-device and qcom,tmd-device yaml bindings.
    
    Qualcomm QMI based TMD cooling device(s) are used for various
    mitigations for remote subsystem(s) including remote processor
    mitigation, rail voltage restriction etc.
    
    Each child node represents one remote subsystem and each child
    of this subsystem in-turn represents separate TMD cooling device.
    
    Cc: daniel.lezcano@linaro.org
    Cc: rafael@kernel.org
    Cc: andersson@kernel.org
    Cc: robh@kernel.org
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@linaro.org>
    (am from https://lore.kernel.org/linux-arm-msm/20220912085049.3517140-4-bhupesh.sharma@linaro.org/)
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@linaro.org>

---
+++ include/dt-bindings/thermal/qcom,tmd.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This header provides constants for the Qualcomm TMD instances.
+ */
+
+#ifndef _DT_BINDINGS_THERMAL_QCOM_TMD_H_
+#define _DT_BINDINGS_THERMAL_QCOM_TMD_H_
+
+#define MODEM0_INSTANCE_ID	0x0
+#define ADSP_INSTANCE_ID	0x1
+#define CDSP_INSTANCE_ID	0x43
+#define SLPI_INSTANCE_ID	0x53
+
+#endif
