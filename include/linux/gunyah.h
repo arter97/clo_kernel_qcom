a120022ee40e089d8d205b3e8de59d00de06b57e
Elliot Berman <quic_eberman@quicinc.com>
2023-06-13 10:20:32 -0700

    FROMLIST: virt: gunyah: Add hypercalls to identify Gunyah
    Add hypercalls to identify when Linux is running a virtual machine under
    Gunyah.
    
    There are two calls to help identify Gunyah:
    
    1. gh_hypercall_get_uid() returns a UID when running under a Gunyah
       hypervisor.
    2. gh_hypercall_hyp_identify() returns build information and a set of
       feature flags that are supported by Gunyah.
    
    Change-Id: Ie4422d7bdbe4ff0774e8ad56dd234578ee20b9ff
    Reviewed-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>
    Link: https://lore.kernel.org/lkml/20230613172054.3959700-5-quic_eberman@quicinc.com/
    Patch-mainline: linux-arm-kernel @ 13/06/23, 17:20
    Signed-off-by: Sayan Dey <quic_sayand@quicinc.com>

--- include/linux/gunyah.h
+++ include/linux/gunyah.h
@@ -6,8 +6,10 @@
 #ifndef _LINUX_GUNYAH_H
 #define _LINUX_GUNYAH_H
 
+#include <linux/bitfield.h>
 #include <linux/errno.h>
 #include <linux/limits.h>
+#include <linux/types.h>
 
 /******************************************************************************/
 /* Common arch-independent definitions for Gunyah hypercalls                  */
@@ -79,5 +81,34 @@ static inline int gh_error_remap(enum gh_error gh_error)
 		return -EINVAL;
 	}
 }
+
+enum gh_api_feature {
+	GH_FEATURE_DOORBELL 	= 1,
+	GH_FEATURE_MSGQUEUE 	= 2,
+	GH_FEATURE_VCPU 	= 5,
+	GH_FEATURE_MEMEXTENT 	= 6,
+};
+
+bool arch_is_gh_guest(void);
+
+#define GH_API_V1			1
+
+/* Other bits reserved for future use and will be zero */
+#define GH_API_INFO_API_VERSION_MASK	GENMASK_ULL(13, 0)
+#define GH_API_INFO_BIG_ENDIAN		BIT_ULL(14)
+#define GH_API_INFO_IS_64BIT		BIT_ULL(15)
+#define GH_API_INFO_VARIANT_MASK	GENMASK_ULL(63, 56)
+
+struct gh_hypercall_hyp_identify_resp {
+	u64 api_info;
+	u64 flags[3];
+};
+
+static inline u16 gh_api_version(const struct gh_hypercall_hyp_identify_resp *gh_api)
+{
+	return FIELD_GET(GH_API_INFO_API_VERSION_MASK, gh_api->api_info);
+}
+
+void gh_hypercall_hyp_identify(struct gh_hypercall_hyp_identify_resp *hyp_identity);
 
 #endif
