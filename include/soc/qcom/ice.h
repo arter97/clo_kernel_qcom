f5257430ef464b85db316eda2f2b1d34183eba4c
Gaurav Kashyap <quic_gaurkash@quicinc.com>
2023-11-21 21:38:14 -0800

    FROMLIST: soc: qcom: support for generate, import and prepare key
    Implements the ICE apis for generate, prepare and import key
    apis and hooks it up the scm calls defined for them.
    Key management has to be done from Qualcomm Trustzone as only
    it can interface with HWKM.
    
    Signed-off-by: Gaurav Kashyap <quic_gaurkash@quicinc.com>
    Link: https://lore.kernel.org/linux-arm-msm/20231122053817.3401748-1-quic_gaurkash@quicinc.com/.
    
    Change-Id: I5b8ea5904a73dc3019f53bb65992c145979f5c23
    Signed-off-by: Seshu Madhavi Puppala <quic_spuppala@quicinc.com>

--- include/soc/qcom/ice.h
+++ include/soc/qcom/ice.h
@@ -39,5 +39,13 @@ bool qcom_ice_hwkm_supported(struct qcom_ice *ice);
 int qcom_ice_derive_sw_secret(struct qcom_ice *ice, const u8 wrapped_key[],
 			      unsigned int wrapped_key_size,
 			      u8 sw_secret[BLK_CRYPTO_SW_SECRET_SIZE]);
+int qcom_ice_generate_key(struct qcom_ice *ice,
+			  u8 lt_key[BLK_CRYPTO_MAX_HW_WRAPPED_KEY_SIZE]);
+int qcom_ice_prepare_key(struct qcom_ice *ice,
+			 const u8 *lt_key, size_t lt_key_size,
+			 u8 eph_key[BLK_CRYPTO_MAX_HW_WRAPPED_KEY_SIZE]);
+int qcom_ice_import_key(struct qcom_ice *ice,
+			const u8 *imp_key, size_t imp_key_size,
+			u8 lt_key[BLK_CRYPTO_MAX_HW_WRAPPED_KEY_SIZE]);
 struct qcom_ice *of_qcom_ice_get(struct device *dev);
 #endif /* __QCOM_ICE_H__ */
