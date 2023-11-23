5d0152a107d3eb6317e0bcb125d4b7993adc9eaa
Shivendra Pratap <quic_spratap@quicinc.com>
2023-11-23 15:37:27 +0530

    QCLINUX: soc: qcom: Implement a driver to write kaslr addr to imem
    Implement a driver to write kaslr address to an imem offset. This
    address is read and used by debug tools that require loading of
    vmlinux and ram dumps.
    
    Change-Id: Iaf1bceb4f95c37c48787e21b8f80f69b674f9f8e
    Signed-off-by: Shivendra Pratap <quic_spratap@quicinc.com>

---
+++ drivers/soc/qcom/qcom_soc_debug.c
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#include <linux/module.h>
+#include <linux/of_address.h>
+
+#define KASLR_IMEM_ADDR_NAME	"qcom,msm-imem-kaslr_offset"
+#define KASLR_IMEM_MAGIC	0xdead4ead
+#define KASLR_OFFSET_MASK	0x00000000FFFFFFFF
+
+static void __iomem *get_iomap_addr(const char *prop_name)
+{
+	struct device_node *node;
+	void __iomem *ioaddr;
+
+	node = of_find_compatible_node(NULL, NULL, prop_name);
+	if (!node) {
+		pr_err("DT property - read error: %s\n", prop_name);
+		return NULL;
+	}
+
+	ioaddr = of_iomap(node, 0);
+	if (!ioaddr)
+		pr_err("DT property - map fail: %s\n", prop_name);
+
+	return ioaddr;
+}
+
+static void store_kaslr_offset(void)
+{
+	void __iomem *imem_kaslr_addr = get_iomap_addr(KASLR_IMEM_ADDR_NAME);
+
+	if (!imem_kaslr_addr)
+		return;
+
+	__raw_writel(KASLR_IMEM_MAGIC, imem_kaslr_addr);
+	__raw_writel((kimage_vaddr - KIMAGE_VADDR) & KASLR_OFFSET_MASK,
+		     imem_kaslr_addr + 4);
+	__raw_writel(((kimage_vaddr - KIMAGE_VADDR) >> 32) & KASLR_OFFSET_MASK,
+		     imem_kaslr_addr + 8);
+
+	iounmap(imem_kaslr_addr);
+}
+
+static int __init qcom_soc_debug_init(void)
+{
+	store_kaslr_offset();
+	return 0;
+}
+
+#if IS_MODULE(CONFIG_QCOM_SOC_DEBUG)
+module_init(qcom_soc_debug_init);
+#else
+pure_initcall(qcom_soc_debug_init);
+#endif
+
+MODULE_DESCRIPTION("QCOM SOC Debug Driver");
+MODULE_LICENSE("GPL");
