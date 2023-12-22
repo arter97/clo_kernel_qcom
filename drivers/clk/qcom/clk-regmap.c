bittencourtjulival/eclipse_kernel_xiaomi_stone

clk: qcom: clk-regmap: Add support for devm_clk_regmap_list_node
There can be cases where the clock providers have multiple regmaps and
directly registers to the clock framework. The clk regmap ops like list
registers/clk rate max would be registered only if the provider is added
to the regmap list node.
Thus expose this API for those providers to add to the list.

Change-Id: I1463c12eeed431d30e96404f2162dd75d202d4b9
Signed-off-by: Taniya Das <tdas@codeaurora.org>

drivers/clk/qcom/clk-regmap.c
@@ -286,6 +286,19 @@ int devm_clk_register_regmap(struct device *dev, struct clk_regmap *rclk)
 }
 EXPORT_SYMBOL_GPL(devm_clk_register_regmap);
 
+/**
+ * devm_clk_regmap_list_node - Add a clk-regmap clock list for providers
+ *
+ * @rclk: clk to operate on
+ *
+ * Maintain clk-regmap clks list for providers use.
+ */
+void devm_clk_regmap_list_node(struct device *dev, struct clk_regmap *rclk)
+{
+	list_add(&rclk->list_node, &clk_regmap_list);
+}
+EXPORT_SYMBOL(devm_clk_regmap_list_node);
+
 int clk_runtime_get_regmap(struct clk_regmap *rclk)
 {
 	int ret;

arter97/clo_kernel_qcom

Signed-off-by: Shubham Diwane <quic_sanantad@quicinc.com>

drivers/clk/qcom/clk-regmap.c
@@ -310,6 +310,19 @@ int devm_clk_register_regmap(struct device *dev, struct clk_regmap *rclk)
 }
 EXPORT_SYMBOL_GPL(devm_clk_register_regmap);
 
+/**
+ * devm_clk_regmap_list_node - Add a clk-regmap clock list for providers
+ *
+ * @rclk: clk to operate on
+ *
+ * Maintain clk-regmap clks list for providers use.
+ */
+void devm_clk_regmap_list_node(struct device *dev, struct clk_regmap *rclk)
+{
+	list_add(&rclk->list_node, &clk_regmap_list);
+}
+EXPORT_SYMBOL_GPL(devm_clk_regmap_list_node);
+
 int clk_runtime_get_regmap(struct clk_regmap *rclk)
 {
 	int ret;