9c7921d9d28cd5ae22a8bebecbd1c3bedc666734
Jaswanth <quic_koppula@quicinc.com>
2024-01-18 11:27:41 +0530

    soc: qcom: Add imem support for reboot reason driver
    Add reboot bootloader and reboot recovery support
    using imem for devices that do not support nvmem cell.
    
    Change-Id: I9018649aef36267011769e85803807d5fc71fd78
    Signed-off-by: Jaswanth <quic_koppula@quicinc.com>

--- drivers/power/reset/qcom-reboot-reason.c
+++ drivers/power/reset/qcom-reboot-reason.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2019, 2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/err.h>
@@ -18,20 +19,22 @@ struct qcom_reboot_reason {
 	struct device *dev;
 	struct notifier_block reboot_nb;
 	struct nvmem_cell *nvmem_cell;
+	void __iomem *imem_restart_addr;
 };
 
 struct poweroff_reason {
 	const char *cmd;
 	unsigned char pon_reason;
+	unsigned int imem_reason;
 };
 
 static struct poweroff_reason reasons[] = {
-	{ "recovery",			0x01 },
-	{ "bootloader",			0x02 },
-	{ "rtc",			0x03 },
-	{ "dm-verity device corrupted",	0x04 },
-	{ "dm-verity enforcing",	0x05 },
-	{ "keys clear",			0x06 },
+	{ "recovery",			0x01, 0x77665502 },
+	{ "bootloader",			0x02, 0x77665500 },
+	{ "rtc",			0x03, 0x77665503 },
+	{ "dm-verity device corrupted",	0x04, 0x77665508 },
+	{ "dm-verity enforcing",	0x05, 0x77665509 },
+	{ "keys clear",			0x06, 0x7766550a },
 	{}
 };
 
@@ -47,9 +50,16 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 		return NOTIFY_OK;
 	for (reason = reasons; reason->cmd; reason++) {
 		if (!strcmp(cmd, reason->cmd)) {
-			nvmem_cell_write(reboot->nvmem_cell,
-					 &reason->pon_reason,
-					 sizeof(reason->pon_reason));
+			if (reboot->nvmem_cell) {
+				nvmem_cell_write(reboot->nvmem_cell,
+						 &reason->pon_reason,
+						 sizeof(reason->pon_reason));
+			} else {
+				if (reboot->imem_restart_addr)
+					__raw_writel(reason->imem_reason,
+						     reboot->imem_restart_addr);
+			}
+
 			break;
 		}
 	}
@@ -60,6 +70,7 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 static int qcom_reboot_reason_probe(struct platform_device *pdev)
 {
 	struct qcom_reboot_reason *reboot;
+	struct device_node *np;
 
 	reboot = devm_kzalloc(&pdev->dev, sizeof(*reboot), GFP_KERNEL);
 	if (!reboot)
@@ -69,8 +80,22 @@ static int qcom_reboot_reason_probe(struct platform_device *pdev)
 
 	reboot->nvmem_cell = nvmem_cell_get(reboot->dev, "restart_reason");
 
-	if (IS_ERR(reboot->nvmem_cell))
-		return PTR_ERR(reboot->nvmem_cell);
+	if (IS_ERR(reboot->nvmem_cell)) {
+		/* For some of old target like mdm9607 uses IMEM to save reboot reason. */
+		np = of_find_compatible_node(NULL, NULL,
+					     "qcom,msm-imem-restart_reason");
+		if (!np) {
+			dev_err(reboot->dev, "Missing qcom,msm-imem-restart_reason node\n");
+			return -ENODEV;
+		}
+		reboot->imem_restart_addr = of_iomap(np, 0);
+		if (!reboot->imem_restart_addr) {
+			dev_err(reboot->dev, "Unable to map qcom,msm-imem-restart_reason offset\n");
+			return -ENOMEM;
+		}
+		of_node_put(np);
+		reboot->nvmem_cell = NULL;
+	}
 
 	reboot->reboot_nb.notifier_call = qcom_reboot_reason_reboot;
 	reboot->reboot_nb.priority = 255;
@@ -86,6 +111,10 @@ static int qcom_reboot_reason_remove(struct platform_device *pdev)
 	struct qcom_reboot_reason *reboot = platform_get_drvdata(pdev);
 
 	unregister_reboot_notifier(&reboot->reboot_nb);
+	if (reboot->nvmem_cell)
+		nvmem_cell_put(reboot->nvmem_cell);
+	if (reboot->imem_restart_addr)
+		iounmap(reboot->imem_restart_addr);
 
 	return 0;
 }
