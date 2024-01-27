0a82efab7153add552d991c7ca0e1acaa9c0fd39
Prasadarao Durvasula <quic_pdurvasu@quicinc.com>
2024-01-27 17:23:57 +0530

    net: ethernet: qualcomm: Add rmnet_data and rmnet_bam.
    Add snapshot of the rmnet_data driver taken as of msm-4.14
    with commit id fbb39fc2c8a2
    (net: rmnet_data: ignore pkts coming with wrong epconfig).
    
    Signed-off-by: Prasadarao Durvasula <quic_pdurvasu@quicinc.com>
    Change-Id: I38fbe21e395f2dca79883322ce14e6a9b0ab5eaf

---
+++ drivers/net/ethernet/qualcomm/rmnet_data/net_map.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _NET_MAP_H_
+#define _NET_MAP_H_
+
+#define RMNET_IP_VER_MASK 0xF0
+#define RMNET_IPV4        0x40
+#define RMNET_IPV6        0x60
+
+#endif /* _NET_MAP_H_ */
