6280ae741b3b662108c378b4114f321e1908f069
Divy Patel <quic_divypate@quicinc.com>
2024-06-13 22:29:25 -0700

    Revert "drivers:remoteproc: qcom_q6v5_mss: Initialize ret variable value"
    This reverts commit 4e35fa6b7379281cbbc63bed1d37fe425c62796b.
    
    Reason for revert: No Need 
    
    Change-Id: I498a263d3c0e33ce4b6c3850b29839487d6f1141
    Signed-off-by: Divy Patel <quic_divypate@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_mss.c
+++ drivers/remoteproc/qcom_q6v5_mss.c
@@ -443,7 +443,7 @@ static int q6v5_load(struct rproc *rproc, const struct firmware *fw)
 
 static int q6v5_reset_assert(struct q6v5 *qproc)
 {
-	int ret = 0;
+	int ret;
 
 	if (qproc->has_alt_reset) {
 		reset_control_assert(qproc->pdc_reset);
