bf33400c121cb9d0b67a3759b5739e51910b2ce8
Samuel Lee <quic_samuelee@quicinc.com>
2024-06-28 13:24:47 +0900

    pinctrl: qcom: fix compile error due to duplicated definition
    conflict due to propagation upstream patch, fix compile error
     - I557cc660b2b2daac05881f6164f89f32c92ef98c
    
    Change-Id: I6dd6ea5cadd4f6df23e29cf3e0d1b972fd896c31
    Signed-off-by: Samuel Lee <quic_samuelee@quicinc.com>

--- drivers/pinctrl/qcom/pinctrl-msm.h
+++ drivers/pinctrl/qcom/pinctrl-msm.h
@@ -103,7 +103,6 @@ struct msm_pingroup {
 	unsigned intr_target_width:5;
 	unsigned intr_wakeup_enable_bit:5;
 	unsigned intr_wakeup_present_bit:5;
-	unsigned intr_target_width:5;
 	unsigned intr_target_kpss_val:5;
 	unsigned intr_raw_status_bit:5;
 	unsigned intr_polarity_bit:5;
