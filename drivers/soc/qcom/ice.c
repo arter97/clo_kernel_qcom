f98ce2cca16358b1b957684ef965d67de131c943
Seshu Madhavi Puppala <quic_spuppala@quicinc.com>
2024-08-23 13:57:16 +0530

    PENDING: qcom: ice: Remove ice probe
    ICE is a part of storage platform device,
    there is no necessity for a separate ICE probe.
    Storage probe will check for the ice node and enable
    crypto capabilities. Having ICE probe will cause crash
    for targets with both emmc and ufs support since probe
    will be called for both ICE nodes irrespective of device
    boot type.
    
    Change-Id: Ia6faa4f1767bac5512df72eadeb2eb64d017a78d
    Signed-off-by: Seshu Madhavi Puppala <quic_spuppala@quicinc.com>

--- drivers/soc/qcom/ice.c
+++ drivers/soc/qcom/ice.c
@@ -611,7 +611,13 @@ struct qcom_ice *of_qcom_ice_get(struct device *dev)
 		goto out;
 	}
 
-	ice = platform_get_drvdata(pdev);
+	base = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(base)) {
+		dev_warn(&pdev->dev, "ICE registers not found\n");
+		return PTR_ERR(base);
+	}
+
+	ice = qcom_ice_create(&pdev->dev, base);
 	if (!ice) {
 		dev_err(dev, "Cannot get ice instance from %s\n",
 			dev_name(&pdev->dev));
@@ -637,45 +643,6 @@ out:
 	return ice;
 }
 EXPORT_SYMBOL_GPL(of_qcom_ice_get);
-
-static int qcom_ice_probe(struct platform_device *pdev)
-{
-	struct qcom_ice *engine;
-	void __iomem *base;
-
-	base = devm_platform_ioremap_resource(pdev, 0);
-	if (IS_ERR(base)) {
-		dev_warn(&pdev->dev, "ICE registers not found\n");
-		return PTR_ERR(base);
-	}
-
-	engine = qcom_ice_create(&pdev->dev, base);
-
-	if (IS_ERR(engine)) {
-		qcom_ice_create_error = true;
-		return PTR_ERR(engine);
-	}
-
-	platform_set_drvdata(pdev, engine);
-
-	return 0;
-}
-
-static const struct of_device_id qcom_ice_of_match_table[] = {
-	{ .compatible = "qcom,inline-crypto-engine" },
-	{ },
-};
-MODULE_DEVICE_TABLE(of, qcom_ice_of_match_table);
-
-static struct platform_driver qcom_ice_driver = {
-	.probe	= qcom_ice_probe,
-	.driver = {
-		.name = "qcom-ice",
-		.of_match_table = qcom_ice_of_match_table,
-	},
-};
-
-module_platform_driver(qcom_ice_driver);
 
 MODULE_DESCRIPTION("Qualcomm Inline Crypto Engine driver");
 MODULE_LICENSE("GPL");
