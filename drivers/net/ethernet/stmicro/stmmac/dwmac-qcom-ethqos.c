668a44c5ba0e28be6bcf184867a38cf9525b5746
Suraj Jaiswal <quic_jsuraj@quicinc.com>
2024-09-04 04:47:18 -0700

    Revert "net: stmmac: Debug patch"
    This reverts commit c8aa162d4190f18f71de79a9a794920680fde1ab.
    
    Change-Id: Ida5b1ec33b27b17b58fb7565cf61850263385ebe
    Signed-off-by: Suraj Jaiswal <quic_jsuraj@quicinc.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -2424,11 +2424,6 @@ static int qcom_ethqos_remove(struct platform_device *pdev)
 	int ret;
 	struct stmmac_priv *priv;
 
-	if (of_device_is_compatible(pdev->dev.of_node, "qcom,emac-smmu-embedded")) {
-		of_platform_depopulate(&pdev->dev);
-		return 0;
-	}
-
 	ethqos = get_stmmac_bsp_priv(&pdev->dev);
 	if (!ethqos)
 		return -ENODEV;
@@ -2436,13 +2431,6 @@ static int qcom_ethqos_remove(struct platform_device *pdev)
 	priv = qcom_ethqos_get_priv(ethqos);
 
 	ret = stmmac_pltfr_remove(pdev);
-
-	if (ethqos->rgmii_clk)
-		clk_disable_unprepare(ethqos->rgmii_clk);
-	if (priv->plat->has_gmac4 && ethqos->phyaux_clk)
-		clk_disable_unprepare(ethqos->phyaux_clk);
-	if (priv->plat->has_gmac4 && ethqos->sgmiref_clk)
-		clk_disable_unprepare(ethqos->sgmiref_clk);
 
 	if (priv->plat->phy_intr_en_extn_stm)
 		free_irq(ethqos->phy_intr, ethqos);
@@ -2455,19 +2443,7 @@ static int qcom_ethqos_remove(struct platform_device *pdev)
 	ethqos_disable_regulators(ethqos);
 	ethqos_clks_config(ethqos, false);
 
-	platform_set_drvdata(pdev, NULL);
-	of_platform_depopulate(&pdev->dev);
 	return ret;
-}
-
-static void qcom_ethqos_shutdown_main(struct platform_device *pdev)
-{
-	struct net_device *dev = platform_get_drvdata(pdev);
-
-	if (!dev)
-	return;
-
-	qcom_ethqos_remove(pdev);
 }
 
 static int qcom_ethqos_suspend(struct device *dev)
@@ -2599,32 +2575,8 @@ static int qcom_ethqos_enable_clks(struct qcom_ethqos *ethqos, struct device *de
 			goto error_rgmii_get;
 		}
 	}
-	ethqos->sgmiref_clk = devm_clk_get(dev, "sgmi_ref");
-	if (IS_ERR(ethqos->sgmiref_clk)) {
-		dev_warn(dev, "Failed sgmi_ref\n");
-		ret = PTR_ERR(ethqos->sgmiref_clk);
-		goto error_sgmi_ref;
-	} else {
-		ret = clk_prepare_enable(ethqos->sgmiref_clk);
-		if (ret)
-			goto error_sgmi_ref;
-	}
-	ethqos->phyaux_clk = devm_clk_get(dev, "phyaux");
-	if (IS_ERR(ethqos->phyaux_clk)) {
-		dev_warn(dev,  "Failed phyaux\n");
-		ret = PTR_ERR(ethqos->phyaux_clk);
-		goto error_phyaux_ref;
-	} else {
-		ret = clk_prepare_enable(ethqos->phyaux_clk);
-		if (ret)
-			goto error_phyaux_ref;
-	}
 	return 0;
 
-error_phyaux_ref:
-	clk_disable_unprepare(ethqos->sgmiref_clk);
-error_sgmi_ref:
-	clk_disable_unprepare(ethqos->rgmii_clk);
 error_rgmii_get:
 	clk_disable_unprepare(priv->plat->pclk);
 error_pclk_get:
@@ -2646,12 +2598,6 @@ static void qcom_ethqos_disable_clks(struct qcom_ethqos *ethqos, struct device *
 
 	if (ethqos->rgmii_clk)
 		clk_disable_unprepare(ethqos->rgmii_clk);
-
-	if (priv->plat->has_gmac4 && ethqos->phyaux_clk)
-		clk_disable_unprepare(ethqos->phyaux_clk);
-
-	if (priv->plat->has_gmac4 && ethqos->sgmiref_clk)
-		clk_disable_unprepare(ethqos->sgmiref_clk);
 
 	ETHQOSINFO("Exit\n");
 }
@@ -2785,7 +2731,6 @@ static const struct dev_pm_ops qcom_ethqos_pm_ops = {
 static struct platform_driver qcom_ethqos_driver = {
 	.probe  = qcom_ethqos_probe,
 	.remove = qcom_ethqos_remove,
-	.shutdown = qcom_ethqos_shutdown_main,
 	.driver = {
 		.name           = DRV_NAME,
 		.pm             = &qcom_ethqos_pm_ops,
