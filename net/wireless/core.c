1e22669911dc724790d8ae10e9bca3104dcc9dad
Vinay Gannevaram <vganneva@qti.qualcomm.com>
2026-04-29 12:39:27 +0530

    ANDROID: Fix opencount tracking for NAN and P2P netdev interfaces
    Some drivers create a NAN or P2P interface as a netdev rather than a
    virtual interface. In that case rdev->opencount is incremented by
    the NETDEV_UP notifier and must be decremented by the NETDEV_DOWN
    notifier.  However cfg80211_stop_nan(), called via cfg80211_leave() on
    NETDEV_GOING_DOWN, also decrements opencount unconditionally, resulting
    in a double decrement and an imbalanced opencount that prevents
    wiphy_unregister() from completing.
    
    The opencount decrement in cfg80211_nan_stop() and cfg80211_p2p_stop()
    is intended only for non-netdev interfaces. Update the logic to invoke
    these APIs exclusively for non-netdev interfaces
    
    Bug: 505636388
    Fixes: e1696c8bd0056 ("wifi: cfg80211: stop NAN and P2P in cfg80211_leave")
    Link: https://lore.kernel.org/all/20260228181736.1605592-63-sashal@kernel.org/
    
    Change-Id: Id81fa37184f62c1f586187e1f17a29b1c2a59f44
    Signed-off-by: raj katariya <rkatariy@qti.qualcomm.com>
    Signed-off-by: Sairam Udugula <sudugula@qti.qualcomm.com>

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -1355,10 +1355,12 @@ void __cfg80211_leave(struct cfg80211_registered_device *rdev,
 		__cfg80211_leave_ocb(rdev, dev);
 		break;
 	case NL80211_IFTYPE_P2P_DEVICE:
-		cfg80211_stop_p2p_device(rdev, wdev);
+		if (!wdev->netdev)
+			cfg80211_stop_p2p_device(rdev, wdev);
 		break;
 	case NL80211_IFTYPE_NAN:
-		cfg80211_stop_nan(rdev, wdev);
+		if (!wdev->netdev)
+			cfg80211_stop_nan(rdev, wdev);
 		break;
 	case NL80211_IFTYPE_AP_VLAN:
 	case NL80211_IFTYPE_MONITOR:
